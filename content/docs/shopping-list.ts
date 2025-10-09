584cd42dfdedce3334e7ddf493242bb7b843a3d3
H4RUTY <st22067hr@gm.ibaraki-ct.ac.jp>
2025-10-09 17:53:59 +0900

    refactor: make shopping list responsive

---
+++ content/docs/shopping-list.ts
@@ -0,0 +1,84 @@
+export type ShoppingItem = {
+  id: number;
+  shop: string;
+  name: string;
+  url: string;
+  price: number;
+};
+
+export const shoppingList: ShoppingItem[] = [
+  {
+    id: 1,
+    shop: "Amazon",
+    name: "8x15x45mm リニアベアリング (x2)",
+    url: "https://www.amazon.co.jp/Kozelo-LM8LUU-%E3%83%AA%E3%83%8B%E3%82%A2%E3%83%9C%E3%83%BC%E3%83%AB%E3%83%99%E3%82%A2%E3%83%AA%E3%83%B3%E3%82%B0-CNC%E3%83%9E%E3%82%B7%E3%83%B3-3D%E3%83%97%E3%83%AA%E3%83%B3%E3%82%BF%E3%83%BC%E7%94%A8/dp/B0CBLSKLV5/ref=sr_1_4?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&dib=eyJ2IjoiMSJ9.jVbKDtjgJafjnaf2tOpS04z1-kKCOTTlX-4KGc9ut27DNcDZ8A2CbrbzupFvUuBEcYKce4MPZAqCehYtioekYj1ms8TYlegEurnIwXFlIJgcXNBxVGzOL1cmEH0F-xnAFq12kQJ6almSIeVXK0Y2P3Rl2FzKGpBsuDZQWKwk1iZicNNXRaJIoyYk78yNW_ZZd0FQnlc4d959lTNatAJHOKxzA9pE2Ea9hmzzhpkpJajlPK_y7H5NqQh3MNbMqdW8sVQEOwmytGB-l4ms2LQyH18OV4EZg3WA76Bmh5ejV14.FHtIr37oZTTA6vDeO9hX6_iozXcka6nFQ64owbeRSDI&dib_tag=se&keywords=%E3%83%AA%E3%83%8B%E3%82%A2%E3%83%99%E3%82%A2%E3%83%AA%E3%83%B3%E3%82%B0+45mm&qid=1759486120&s=industrial&sr=1-4",
+    price: 1361,
+  },
+  {
+    id: 2,
+    shop: "モノタロウ",
+    name: "8x15x25mm リニアベアリング (x1)",
+    url: "https://www.monotaro.com/p/0360/7923/?t.q=%E3%83%AA%E3%83%8B%E3%82%A2%E3%83%99%E3%82%A2%E3%83%AA%E3%83%B3%E3%82%B0",
+    price: 289,
+  },
+  {
+    id: 3,
+    shop: "Amazon",
+    name: "タイミングベルトとプーリー",
+    url: "https://www.amazon.co.jp/KeeYees-%E3%82%BF%E3%82%A4%E3%83%9F%E3%83%B3%E3%82%B0%E3%83%99%E3%83%AB%E3%83%88-%E6%AD%AF%E4%BB%98%E3%82%A2%E3%82%A4%E3%83%89%E3%83%A9%E3%83%BC-%E5%9B%BA%E5%AE%9A%E7%94%A8%E3%82%AF%E3%83%A9%E3%83%B3%E3%83%97-3D%E3%83%97%E3%83%AA%E3%83%B3%E3%82%BF%E3%83%91%E3%83%BC%E3%83%84/dp/B07SH7ZYTV/ref=sr_1_5?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=HFMHOMKH981T&dib=eyJ2IjoiMSJ9.Hm8x8X1yqqTO8rsvULQvo_MdevbnjaS0d2d4eKuwo47g36dX0K1gER6Zf8SZW57A3LYbp49XDonoQyt73kzKJv29h7Pm4GqsUJ4ly-8qfFpiHwZoc2lsAfxrWg2QMJuGfuquoQ_GZlMmb1p8Tibve5Lsa6R5EhklAJNp76JZp5T1npN9I3yh_dwuwloxoA9vHAQicBmnAGKZXxZgeJgn63B4ZSdLqsNhhLDuvtFmt-dVvZ6qD6dnrQ_73kq1bSlrYnmyhrWlG0iZ0Kixz2v2fh8RAe6yG8JnbJddavm3pOU._HULQ_KZSWhl_xcqMvF-R23D_AyALEH2fQLHa7A7Mf0&dib_tag=se&keywords=gt2+%E3%82%BF%E3%82%A4%E3%83%9F%E3%83%B3%E3%82%B0%E3%83%99%E3%83%AB%E3%83%88&qid=1759484109&sprefix=gt2+%E3%82%BF%E3%82%A4%E3%83%9F%E3%83%B3%E3%82%B0%E3%83%99%E3%83%AB%E3%83%88%2Caps%2C200&sr=8-5&xpid=uh-n_bzcb0mdh",
+    price: 1820,
+  },
+  {
+    id: 4,
+    shop: "Amazon",
+    name: "コンタクトスイッチ",
+    url: "https://www.amazon.co.jp/Youmile-20%E5%80%8B%E3%81%AE%E3%83%9E%E3%82%A4%E3%82%AF%E3%83%AD%E3%82%B9%E3%82%A4%E3%83%83%E3%83%81%E3%83%AD%E3%83%BC%E3%83%A9%E3%83%BC%E3%83%AC%E3%83%90%E3%83%BC%E3%82%A2%E3%83%BC%E3%83%A03%E3%83%94%E3%83%B3AC-125250V-SPDT-1NC%E3%83%A2%E3%83%BC%E3%83%A1%E3%83%B3%E3%82%BF%E3%83%BCPCB%E7%AB%AF%E5%AD%90%E9%96%8B%E9%96%89%E3%82%B9%E3%82%A4%E3%83%83%E3%83%81/dp/B084VNY6PK/ref=sr_1_1?dib=eyJ2IjoiMSJ9.t1m5I5mOsh71IJKrZihDGpFCXeheOAcvN3BHe55buvF0auv1DBfkut06FKL6ra5RqHW4Z_FwxPBe_A9gp1J5qMnra0rRzAzMJ38VE6UF8uEavHXe4nLM0NgyAqP5Kw8erL9mwKtxz_JrNXTRRphHTHpDZ1LJxk4yBuA2iZ0Q8Z3IlYcZC88lX5vFR29VsmXz_xTzr46T8O2YyoMnmIob2mPh3BE5lx2wMWqa28nxoDgeLyJG_F81g4W3olvYxY2MLBkIwughKRcXxLxtvSgsEi1Sg6-YqAmR061isZQAfmM.2G0SFo3yJ9ETsqTbSOjVld1VUgT4XbPUdOZ7I5BTbHY&dib_tag=se&geniuslink=true&keywords=indusky%2B10pcs%2Bmicro%2Blimit%2Bswitch%2Bwith%2Bmomentary%2Broller%2Blever%2Barm%2Bac%2B250v%2B5a%2Bspdt%2B1no%2B1nc%2Bsnap%2Baction%2Bmicro%2Bswitches&qid=1759475525&sr=8-1&th=1",
+    price: 679,
+  },
+  {
+    id: 5,
+    shop: "Amazon",
+    name: "8mm 木丸棒",
+    url: "https://www.amazon.co.jp/%E3%82%A2%E3%83%BC%E3%83%86%E3%83%83%E3%82%AF-%E4%B8%B8%E6%A3%92-%CE%A68%C3%97450-5%E6%9C%AC%E7%B5%84-46569/dp/B0041NF2PK/ref=pd_sbs_d_sccl_2_13/356-9574604-4637517?pd_rd_w=nT9i8&content-id=amzn1.sym.c0889d40-54df-4be4-b1c5-43522a3d0a92&pf_rd_p=c0889d40-54df-4be4-b1c5-43522a3d0a92&pf_rd_r=DFNQW09S290KJ5V08SR0&pd_rd_wg=m6kmS&pd_rd_r=f2749683-c8ff-43e2-ba28-fa1a164f7bb5&pd_rd_i=B0041NF2PK&psc=1",
+    price: 323,
+  },
+  {
+    id: 6,
+    shop: "Amazon",
+    name: "ブロック付き30cm長リニアレール MGN12H",
+    url: "https://www.amazon.co.jp/Akozon-%E3%83%AA%E3%83%8B%E3%82%A2%E3%82%AC%E3%82%A4%E3%83%89%E3%83%AC%E3%83%BC%E3%83%AB%E3%83%9F%E3%83%8B%E3%83%81%E3%83%A5%E3%82%A2%E3%83%AA%E3%83%8B%E3%82%A2%E3%82%B9%E3%83%A9%E3%82%A4%E3%83%89%E3%83%AC%E3%83%BC%E3%83%AB%E3%82%AC%E3%82%A4%E3%83%89%E3%83%96%E3%83%AD%E3%83%83%E3%82%AF250mm-300mm-400mm-500mm/dp/B07Y4NWB45/ref=sr_1_5?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=R7I7Z8JD5ER3&dib=eyJ2IjoiMSJ9.V2OdZ6jQk0YSq4UbbtIWuL1SlyTyXV1XmZ2W_-vMyQr-c1iwxnLeukinKUG8elchF5W_XN3KRtdJ_g9MdWsX4BBJGfZX9gdnD28dmtmHl2189xjbf2TZa6qwi7zmo9WQNqNOeTabDRfTeC_IYUJw8ymHeso9ohzcOwN3W266PCcFnajY3WDZhlcazDSCPbZtiF5tbCmKMnsEeGD_YFfmk28_HB1B8-dQGpN1kUhr-YbMaEdFjgfn-D2Z3eaqi3NJnZEkr5yG_OlZtaqxz3wteX6cCSSI3bixAMkv1tuApuY.0ZkwR7UbRYsavpRleWw1frI9ykIrFfbpHy3Nry0rME0&dib_tag=se&keywords=%E3%83%AA%E3%83%8B%E3%82%A2%E3%83%AC%E3%83%BC%E3%83%AB%2B300&qid=1759482924&s=industrial&sprefix=%E3%83%AA%E3%83%8B%E3%82%A2%E3%83%AC%E3%83%BC%E3%83%AB%2B300%2Cindustrial%2C308&sr=1-5&th=1",
+    price: 1789,
+  },
+  {
+    id: 7,
+    shop: "Amazon",
+    name: "アイドラーホイール 内径3mm 6mmベルト用",
+    url: "https://www.amazon.co.jp/Saipor-%E3%82%BF%E3%82%A4%E3%83%9F%E3%83%B3%E3%82%B0%E3%83%99%E3%83%AB%E3%83%88%E3%83%97%E3%83%BC%E3%83%AA%E3%83%BC-3D%E3%83%97%E3%83%AA%E3%83%B3%E3%82%BF%E3%83%BC%E4%BA%A4%E6%8F%9B%E7%94%A8-%E3%82%BF%E3%82%A4%E3%83%9F%E3%83%B3%E3%82%B0%E3%83%99%E3%83%AB%E3%83%88%E3%81%AB%E9%81%A9%E7%94%A8-16%E6%AD%AF%E3%80%81%E5%86%85%E5%BE%843mm%E3%80%81%E5%B9%856mm%E3%80%81%E6%AD%AF%E3%81%AA%E3%81%97/dp/B086JPY1WH/ref=sr_1_3?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=2TWVEPIUTZFS6&dib=eyJ2IjoiMSJ9.YBoxkOG6ZEvUoxNA4YFVNsqoRBVUPD8CtEZBBZ-GbXbojfxovgrBa8LlON_W3ekFcRtMbFS7-Yg8WmuuD-_rQgrLj7ptNeqHpasnUpCsTtKLr4HChAL6oRiHb9DT-yFmjQXb40DqNWcxBb-cl-OXAvTlEuWENekkqIvz2siFYftTnDMeSyTRKQugaiEubjIEIHsMjE0RUs8061FO58O-rLHeR0XKHHN-PORRhNUV2w37SPHOMjrETAgAko7ynbvafoMaUnFLkH-rWZG4GaWJNwy5j1lLKyfNdq_phIeGoGg.CQLb6H8Ftl50f1YFCFQKs486Px-71LQYeixG7wZv9CM&dib_tag=se&keywords=%E3%83%97%E3%83%BC%E3%83%AA%E3%83%BC%2B3mm%2B6mm&qid=1759484757&sprefix=%E3%83%97%E3%83%BC%E3%83%AA%E3%83%BC%2B3mm%2B6mm%2Caps%2C182&sr=8-3&th=1",
+    price: 959,
+  },
+  {
+    id: 8,
+    shop: "Amazon",
+    name: "Nema17ステッピングモータ(x4) + CNCシールド + A4988モータードライバ(x4)",
+    url: "https://www.amazon.co.jp/Nema17-%E3%82%B9%E3%83%86%E3%83%83%E3%83%94%E3%83%B3%E3%82%B0-17HS4401S-A4988-%E3%83%89%E3%83%A9%E3%82%A4%E3%83%90%E3%83%BC%E6%8B%A1%E5%BC%B5%E3%83%9C%E3%83%BC%E3%83%89/dp/B0D59RHVPY/ref=sr_1_8?dib=eyJ2IjoiMSJ9.gZ4KcAD86di1PEsmsm4QVbiYSI-3d2RdMe6wLFfiSWdDYA7JqmajQn_SFdQrns9vCXVM14bykgsL9SfQNolDw7zm8U2m8M1MCO4gTgdJTNoEZ6d6NzXV11TNEHDq4rWj-vjEKBN_cwhaejBk6Iu9ac98dEKfSQMftxaLBJbBOoIWCZK0dUMTsjl_TnvmhJDZFHeoxS-Q0ne8MtbWYJhkQpmIQNw2j4Xu3iQ5st0SUu_R4QvkWjGbmYle_cG1ykWs8gdXIjwshz25fCt1hMX4h14rG2_OTMhtwFKr_dgmqhg.netsRNRvH7F9pwq7V_gntQZUJYlKlz3JjbzYc07LLCU&dib_tag=se&geniuslink=true&keywords=nema+17+stepper+motor&qid=1759485098&sr=8-8",
+    price: 5198,
+  },
+  {
+    id: 9,
+    shop: "モノタロウ",
+    name: "80mm ロールペーパー",
+    url: "https://www.monotaro.com/p/3136/2809/?t.q=%E3%83%AD%E3%83%BC%E3%83%AB%E3%83%9A%E3%83%BC%E3%83%91%E3%83%BC%E3%80%8080mm",
+    price: 209,
+  },
+  {
+    id: 10,
+    shop: "モノタロウ",
+    name: "モノタロウ送料",
+    url: "https://help.monotaro.com/app/answers/detail/a_id/19/~/%E9%85%8D%E9%80%81%E6%96%99%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6",
+    price: 500,
+  },
+];
+
+export default shoppingList;
+
+
