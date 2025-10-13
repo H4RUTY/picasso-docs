c83e2858c94457d9108621f8067767cb6756a83e
H4RUTY <st22067hr@gm.ibaraki-ct.ac.jp>
2025-10-13 22:26:55 +0900

    feat: add announcement banner and update docs index UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/global.css';
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import { Zen_Kaku_Gothic_New } from 'next/font/google';
+import { Banner } from 'fumadocs-ui/components/banner';
 
 const zen_kaku_gothic_new = Zen_Kaku_Gothic_New({
   weight: ['400', '700'],
@@ -11,7 +12,15 @@ export default function Layout({ children }: LayoutProps<'/'>) {
   return (
     <html lang="ja" className={zen_kaku_gothic_new.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <RootProvider>
+          <Banner 
+            id="banner"
+            variant='rainbow'
+          >
+            スケジュールページを作成しました！みてね👌
+          </Banner>
+          {children}
+        </RootProvider>
       </body>
     </html>
   );
