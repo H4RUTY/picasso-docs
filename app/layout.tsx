6a84538bfe37574564fe4200e4f1efd52633264e
H4RUTY <st22067hr@gm.ibaraki-ct.ac.jp>
2025-11-20 22:53:49 +0900

    refactor: remove Banner component from layout for cleaner structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import '@/app/global.css';
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import { Zen_Kaku_Gothic_New } from 'next/font/google';
-import { Banner } from 'fumadocs-ui/components/banner';
 
 const zen_kaku_gothic_new = Zen_Kaku_Gothic_New({
   weight: ['400', '700'],
@@ -13,12 +12,6 @@ export default function Layout({ children }: LayoutProps<'/'>) {
     <html lang="ja" className={zen_kaku_gothic_new.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>
-          <Banner 
-            id="schedule"
-            variant='rainbow'
-          >
-            買い物リストを更新しました
-          </Banner>
           {children}
         </RootProvider>
       </body>
